ac9977e47e738d066e85bf767299db4c951d83d9
finnd <finn.daut@t-online.de>
2024-04-14 16:22:29 +0200

    asdf

--- U01_3.py
+++ U01_3.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 def f(x1, x2, a=float(0), b=float(0)):
     ret1 = a * x1
     ret2 = (b - a ** 2) * x1 ** 2 + b * x2
-    return ret1, ret2
+    return [ret1, ret2]
 
 
 nrange = np.arange(26)
 coor_list = []
 c = 1 / 100
-x_n = (c, c ** 2)
+x_n = [c, c ** 2]
 for _ in nrange:
     coor_list.append(x_n)
     x_n = f(*x_n, a=9 / 10, b=10)
