5755239f13227eb6ef0b55e3bd90d5088ebf8921
finndaut <finn.daut@t-online.de>
2024-04-15 15:25:58 +0200

    new file:   xyz.m

---
+++ xyz.m
@@ -0,0 +1,31 @@
+A = [1 2; 3 4]
+B = [2 3; 4 5]
+
+A+B
+A*B
+
+clc
+clear
+
+x_val = 0:0.001:1;
+
+f = @(x) x.^2
+g = @(x) x.^4
+
+figure(1)
+plot(x_val, f(x_val), 'r-', 'linewidth', 2)
+hold on
+plot(x_val, g(x_val), 'b-', 'linewidth', 2)
+title('Schöne Funktion')
+xlabel('x')
+ylabel('y')
+hold off
+
+figure(2)
+loglog(x_val, f(x_val), 'r-', 'linewidth', 2)
+hold on
+loglog(x_val, g(x_val), 'b-', 'linewidth', 2)
+title('Schöne Funktion mit log-Achse')
+xlabel('x')
+ylabel('y')
+hold off
