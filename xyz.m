62ccc9c3bb65b51c9d85e16f21483a91cc2d790c
finndaut <finn.daut@t-online.de>
2024-04-18 09:04:38 +0200

    new file:   octave-workspace 	new file:   testb.m 	modified:   xyz.m

--- xyz.m
+++ xyz.m
@@ -7,10 +7,12 @@ A*B
 clc
 clear
 
-x_val = 0:0.001:1;
+% x_val = 0:0.001:1;
+x_val = linspace(0, 1)
 
 f = @(x) x.^2
 g = @(x) x.^4
+g = @(x, y) [x + y; x .* y];
 
 figure(1)
 plot(x_val, f(x_val), 'r-', 'linewidth', 2)
