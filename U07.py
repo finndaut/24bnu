65ffbaddb832ce0855d3d6c516a41bb29f8679f1
finndaut <finn.daut@t-online.de>
2024-05-29 19:29:14 +0200

    asdf

--- U07.py
+++ U07.py
@@ -22,32 +22,31 @@ def romberg(start):
     T_arr[0, 0] = h_range[1]*(f(start)+f(stop))
     m_n[0] = 0
 
-    for n in range(1,):
+    for n in range(1, 9):
         i_range = np.arange(0, 2**n)
         m_n[n] = h_range[n] * np.sum(f(start + (2 * i_range + 1) * h_range[n+1]))
 
-    for n in n_range[1:10]:
+    for n in range(1, 10):
         T_arr[n, 0] = 1/2 * (T_arr[n-1, 0] + m_n[n-1])
 
-    for k in n_range[1:10]:
+    for k in range(1, 10):
         T_arr[k:, k] = (4**k * T_arr[k:, k-1] - T_arr[k-1:-1,k-1]) / (4**k - 1)
-
 
     return T_arr
 
 
 start = 1.0
 error = abs(romberg(start) - intf(start))
-i_range = np.arange(0,10)
+i_range = np.arange(0, 10)
 
 
 fig, ax = plt.subplots()
 
-for k in [0,1]:
+for k in [0, 1]:
     plot_range = i_range[k:]
-    ax.semilogy(plot_range, error[k,plot_range], label='k=%i' %k)
+    ax.semilogy(plot_range, error[plot_range, k], label='k=%i' %k)
 
-ax.semilogy(i_range, error[i_range,i_range], label='k=i')
+ax.semilogy(i_range, error[i_range, i_range], label='k=i')
 ax.set(xlabel='i', ylabel='Fehler')
 ax.legend()
 plt.show()
