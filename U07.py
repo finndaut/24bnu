2bbbdf89bec71df3c847e41e0e061c316ad6dc58
finndaut <finn.daut@t-online.de>
2024-05-29 19:21:01 +0200

    modified:   U07.ipynb 	modified:   U07.py

--- U07.py
+++ U07.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def f(x): 
@@ -16,24 +17,37 @@ def romberg(start):
     n_range = np.arange(0, 10)
     h_range = (stop - start) * float2 ** -n_range
 
-    T_nn = h_range[1]*(f(start)+f(stop))
     T_arr = np.empty([10,10])
-    T_arr[0, 0] = T_nn
+    m_n = np.empty([10])
+    T_arr[0, 0] = h_range[1]*(f(start)+f(stop))
+    m_n[0] = 0
+
+    for n in range(1,):
+        i_range = np.arange(0, 2**n)
+        m_n[n] = h_range[n] * np.sum(f(start + (2 * i_range + 1) * h_range[n+1]))
 
     for n in n_range[1:10]:
-        i_range = np.arange(0, 2**(n)-1)
-        m_n = h_range[n-1] * np.sum(start + float2 * i_range * h_range[n])
-        T_arr[0, n] = 1/2 * (T_arr[0, n-1] + m_n)
-    
+        T_arr[n, 0] = 1/2 * (T_arr[n-1, 0] + m_n[n-1])
+
     for k in n_range[1:10]:
-        T_arr[k,k:] = (4**k * T_arr[k-1,k:] - T_arr[k-1,k-1:-1]) / (4**k - 1)
+        T_arr[k:, k] = (4**k * T_arr[k:, k-1] - T_arr[k-1:-1,k-1]) / (4**k - 1)
 
 
     return T_arr
 
 
+start = 1.0
+error = abs(romberg(start) - intf(start))
+i_range = np.arange(0,10)
 
-for start in [1.2, 1.005, 1]:
-    error = abs(romberg(start) - intf(start))
-    
 
+fig, ax = plt.subplots()
+
+for k in [0,1]:
+    plot_range = i_range[k:]
+    ax.semilogy(plot_range, error[k,plot_range], label='k=%i' %k)
+
+ax.semilogy(i_range, error[i_range,i_range], label='k=i')
+ax.set(xlabel='i', ylabel='Fehler')
+ax.legend()
+plt.show()
