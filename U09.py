8862e8efedfccad7b970e88b869590cc2a499ae3
finndaut <finn.daut@t-online.de>
2024-06-09 03:38:54 +0200

    asdf

--- U09.py
+++ U09.py
@@ -1,28 +1,37 @@
 import numpy as np
 
 
-#def cholesky(pseudo_mat, )
+def cholesky(matrix):
+    i_dim, _ = np.shape(matrix)
+    g_arr = np.zeros_like(matrix)
+    g_arr[0, 0] = matrix[0, 0] ** (1/2)
+    g_arr[1:, 0] = matrix[1:, 0] / matrix[0, 0] ** (1/2)
+    for i in range(1, i_dim):
+        g_arr[i, i] = (matrix[i, i] - np.sum(matrix[i, 0:i]**2)) ** (1/2)
+        g_arr[i+1:, i] = (matrix[i+1:, i] - np.sum(matrix[i+1:, 0:i] * matrix[i, 0:i], axis=1)) / g_arr[i, i]
+    return g_arr
+
 
 
 def pseudo_a(n, m, dim_m=2):
     if n == m:
         ret = 4
-    elif abs(n - m) == abs(dim_m) or (abs(n - m) == 1 and not ((n%dim_m != 1) and (m%dim_m != 1))):
+    elif abs(n - m) == abs(dim_m) or (abs(n - m) == 1 and not (n+m+1) % (2*dim_m) == 0):
         ret = -1
     else:
         ret = 0
 
-    return ret
+    return np.float64(ret)
 
 
 def pseudo_h(n, m, dim_m=2):
-    return 1 / (n + m + 1)
+    return 1 / np.float64(n + m + 1)
 
 
 pseudo_a = np.vectorize(pseudo_a)
 pseudo_h = np.vectorize(pseudo_h)
 
 
-print(pseudo_a(*np.ogrid[0:3**2, 0:3**2], dim_m=3))
-print(np.arange(9)%3)
-print(np.arange(9)%3)
+a_mat = pseudo_a(*np.ogrid[0:3**2, 0:3**2], dim_m=3)
+g_mat = cholesky(a_mat)
+print(np.linalg.norm(a_mat-g_mat@(g_mat.T)))
