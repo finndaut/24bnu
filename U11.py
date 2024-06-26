ff063c0db3e1a34985bfe07f1f652afe3b4c0d7d
finnd <finn.daut@t-online.de>
2024-06-27 01:18:05 +0200

    modified:   U11.py

--- U11.py
+++ U11.py
@@ -17,14 +17,14 @@ def deriv1(x):
 
 
 def f2(x):
-    return x**3-x
+    return x - x*1j + 1
 
 
 def bisect(func, start, sigma, fehler):
     x_n = start
     ret_list = [x_n]
     if func(x_n + sigma) * func(x_n - sigma) < 0:
-        while abs(func(x_n)) > fehler:
+        while np.linalg.norm(func(x_n)) > fehler:
             if func(x_n + sigma) * func(x_n) < 0:
                 sigma /= 2
                 x_n += sigma
@@ -45,7 +45,7 @@ def sekante(func, start, sigma, fehler):
     x_offset = start - sigma
     x_n = start + sigma
     ret_list = [start - sigma, start + sigma]
-    while abs(func(x_n)) > fehler:
+    while np.linalg.norm(func(x_n)) > fehler:
         x_offset = x_n - (x_n - x_offset) * func(x_n) / (func(x_n) - func(x_offset))
         x_n, x_offset = x_offset, x_n
         ret_list.append(x_n)
@@ -55,12 +55,27 @@ def sekante(func, start, sigma, fehler):
 def newton(func, deriv, start, sigma, fehler):
     x_n = start + sigma
     ret_list = [x_n]
-    while abs(func(x_n)) > fehler:
+    while np.linalg.norm(func(x_n)) > fehler:
         x_n -= func(x_n) / deriv(x_n)
         ret_list.append(x_n)
     return ret_list
 
 
 print(bisect(f0, -3, 0.2, 10 ** -10))
+print(bisect(f1, -15, 0.2, 10 ** -10))
+print(bisect(f1, 0.6, 0.2, 10 ** -10))
+print(bisect(f1, 5, 0.2, 10 ** -10))
+
+
 print(sekante(f0, -3, 0.2, 10 ** -10))
+print(sekante(f1, -15, 0.2, 10 ** -10))
+print(sekante(f1, 0.6, 0.2, 10 ** -10))
+print(sekante(f1, 5, 0.2, 10 ** -10))
+
+
 print(newton(f0, deriv0, -3, 0.2, 10 ** -10))
+print(newton(f1, deriv1, -15, 0.2, 10 ** -10))
+print(newton(f1, deriv1, 0.6, 0.2, 10 ** -10))
+print(newton(f1, deriv1, 5, 0.2, 10 ** -10))
+
+print(sekante(f2, 0, 1, 10 ** -10))
